bbd5d5c4cc0202b2fe8cc8208dec401080dffd94
Ryan Baxter <rbaxter@pivotal.io>
2016-09-08 13:36:11 -0400

    added error handling

--- src/main/java/com/ryanjbaxter/spring/cloud/ocr/participants/OcrParticipantsApplication.java
+++ src/main/java/com/ryanjbaxter/spring/cloud/ocr/participants/OcrParticipantsApplication.java
@@ -1,7 +1,9 @@
 package com.ryanjbaxter.spring.cloud.ocr.participants;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.HealthEndpoint;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.Status;
@@ -9,8 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -28,6 +32,9 @@ public class OcrParticipantsApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(OcrParticipantsApplication.class, args);
     }
+
+    @Autowired
+	public HealthEndpoint healthEndpoint;
     
     @Bean
     public AlwaysSampler defaultSampler() {
@@ -42,13 +49,23 @@ public class OcrParticipantsApplication implements CommandLineRunner {
 	
 	@RequestMapping("/")
 	public List<Participant> getParticipants() {
+		if(!healthEndpoint.invoke().getStatus().equals(Status.UP)) {
+			throw new OutOfServiceException();
+		}
 		return participants;
 	}
 	
 	@RequestMapping("/races/{id}")
 	public List<Participant> getParticipants(@PathVariable String id) {
+		if(!healthEndpoint.invoke().getStatus().equals(Status.UP)) {
+			throw new OutOfServiceException();
+		}
 		return participants.stream().filter(p -> p.getRaces().contains(id)).collect(Collectors.toList());
 	}
+
+	@ResponseStatus(value= HttpStatus.INTERNAL_SERVER_ERROR, reason="Server unhealthy")
+	static class OutOfServiceException extends RuntimeException {}
+
 }
 
 class Participant {
